8a708c2d932849e819a13ef8de816ad3ec02bdcd
powermeen <powermeen1@gmail.com>
2017-06-18 22:15:09 +0700

    update

---
+++ StaterProject/src/main/java/com/common/constant/ApplicationConstant.java
@@ -0,0 +1,13 @@
+package com.common.constant;
+
+public interface ApplicationConstant {
+
+	public String  DIRECTION_SEARCH = "search";
+	
+	public String  DIRECTION_INSERT = "insert";
+	
+	public String  DIRECTION_UPDATE = "update";
+	
+	public String  DIRECTION_DELETE = "delete";
+	
+}
